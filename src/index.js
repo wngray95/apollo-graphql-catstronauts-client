9c62ad0973416e549a641db71c8f52d5e960d880
Gray-William <gray_william@bah.com>
2023-01-02 12:20:18 -0500

    updating apollo server uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'https://graphql-catstronauts-server-production.up.railway.app/', // change this to YOUR server URI
+    uri: 'https://graphql-catstronauts-server-production.up.railway.app/',
     cache: new InMemoryCache()
   });
 
